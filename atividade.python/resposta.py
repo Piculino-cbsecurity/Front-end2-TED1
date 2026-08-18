05376d8ece7971125ff59c6c9083d041faae2dbe
Piculino-cbsecurity <piculino.gamer@gmail.com>
2026-08-18 19:57:15 -0300

    Add files via upload

---
+++ atividade.python/resposta.py
@@ -0,0 +1,23 @@
+Alturas = []
+Soma = 0
+Masculino = 0
+Feminino = 0
+
+for i in range(4):
+    altura = float(input("Digite a altura: "))
+    genero = input("Digite o genero: ").lower()
+
+    Alturas.append(altura)
+
+    if genero == "masculino":
+        Soma += altura
+        Masculino += 1
+    else:
+        Feminino += 1
+
+media = int((Soma / Masculino) * 100) / 100
+
+print("Maior altura:", max(Alturas))
+print("Menor altura:", min(Alturas))
+print("Media dos homens:", media)
+print("Quantidade de mulheres:", Feminino)
